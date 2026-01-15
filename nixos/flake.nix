82a5b35a7ea5ce4cb8ef41ba17f3bf334f29c545
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-01-15 18:06:15 +0900

    Update flake.nix (add nixvim)

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,13 +3,14 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		zen-browser.url = "github:youwen5/zen-browser-flake";
+                nixvim.url = "github:nix-community/nixvim";
 		zapret-discord-youtube.url = "github:kartavkun/zapret-discord-youtube";
 		home-manager = {
 			url = "github:nix-community/home-manager/release-25.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
-	outputs = { self, nixpkgs, zen-browser, zapret-discord-youtube, ... }@inputs:
+	outputs = { self, nixpkgs, zen-browser, zapret-discord-youtube, nixvim, ... }@inputs:
 		let
 			system = "x86_64-linux";
 		in{
@@ -19,6 +20,7 @@
 			modules = [
 			./configuration.nix
 			
+                        nixvim.nixosModules.nixvim 
 			zapret-discord-youtube.nixosModules.default
         		{
         			services.zapret-discord-youtube = {
