23865dda336a987fe63912ac41b22d12a4f8465c
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-01-15 18:03:29 +0900

    Create nixvim.nix

---
+++ nixos/modules/nixvim.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }:
+
+{
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+
+    keymaps = [
+      # Открыть Neo-tree по нажатию Ctrl+n
+      {
+        mode = "n";
+        key = "<C-n>";
+        action = ":Neotree toggle<CR>";
+        options.silent = true;
+      }
+
+      # Быстрое сохранение через Space + w
+      {
+        mode = "n";
+        key = "<leader>w";
+        action = ":w<CR>";
+        options.desc = "Save file";
+      }
+    ];
+
+    plugins = {
+      vim-devicons.enable = true;
+      neo-tree.enable = true;
+      lightline = {
+        enable = true;
+	settings = {
+	  colorscheme = "Tomorrow_Night_Bright";
+	  separator = {
+            left = "";
+            right = "";
+          };
+          subseparator = {
+            left = "";
+            right = "";
+          };
+ 	    # Настройка строк состояния (активной и неактивной)
+          active = {
+            left = [
+              [ "mode" "paste" ]
+              [ "readonly" "filename" "modified" "method" ]
+            ];
+            right = [
+              [ "lineinfo" ]
+              [ "percent" ]
+              [ "fileformat" "fileencoding" "filetype" ]
+            ];
+          };
+	};
+      };
+    };
+  };
+}
