673c3985c700967788876643ed32ef51ddc8f069
Rexilone <117955767+Rexilone@users.noreply.github.com>
2026-01-20 23:12:59 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     set -g fish_greeting ""
-    abbr -a ff 'fastfetch'
     abbr -a rebuild 'sudo nixos-rebuild switch --flake /etc/nixos'
+    alias ff="fastfetch"
+    alias n="nvim"
 end
